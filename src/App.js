5d7ca5378d019b9575a53c74b05f46d230892367
saif <mohamedhawas123@gmail.com>
2020-12-26 00:27:13 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Signup from './pages/signup'
 import ButtonAppBar from './components/Navbar'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
      <Router>
-       <ButtonAppBar />
-       <switch>
+     <ButtonAppBar />
+       <div className="container">
+         
+       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact  path="/signup" component={Signup} />
        <Route exact  path="/login" component={Login} />
        
-       </switch>
+       </Switch>
+       </div>
+      
       
      </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-
+import Button from '@material-ui/core/Button';
 
 class Navbar extends Component {
     render() {
         return (
             <AppBar>
                 <Toolbar>
-                    
+                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Sign Up</Button>
                 </Toolbar>
             </AppBar>
             
