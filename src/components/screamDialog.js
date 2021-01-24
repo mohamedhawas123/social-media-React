04118b9f9ae3d36afb234668536dca46ad6b45a0
saif <mohamedhawas123@gmail.com>
2021-01-25 00:06:31 +0200

    scream detail

--- src/components/screamDialog.js
+++ src/components/screamDialog.js
@@ -17,11 +17,35 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import CloseIcon from '@material-ui/icons/Close'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import UnfoldMore from '@material-ui/icons/UnfoldMore'
 import {fetchoneSCream} from '../store/actions/getScream'
 
-const styles = {
+const styles = theme => ({
+    ...theme.spreadThis,
+    invis: {
+        border : 'none',
+        margin: 4
+    },
+    profileImage: {
+        width : '100%',
+        height: "100%",
+        borderRadius: '50%',
+        ObjectFit: 'cover'
+    },
+    DialogContent: {
+        padding : 20
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '90%'
+    },
+    spinner: {
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 50
+    }
 
-}
+})
 
 class ScreamDialog extends Component {
 
@@ -40,10 +64,65 @@ class ScreamDialog extends Component {
         this.setState({open: false})
     }
 
+
     render() {
         const {classes} = this.props
+        const  {userHandle, body, createdAt, userImage} = this.props.scream
+        const dialogMarkUP = this.props.loading ? (
+            <div className={classes.spinner} >
+                <CircularProgress size={200} />
+            </div>
+        ): (
+            <Grid container spacing={16}>
+                <Grid item sm={5}>
+                    <img src={userImage} alt="Profile" className={classes.profileImage} />
+                </Grid>
+                <Grid item sm={7}>
+                    <Typography
+                    component={Link}
+                    color="primary"
+                    variant="h5"
+                    to={`/users/${userHandle}`}>
+                        @{userHandle}
+                    </Typography>
+                    <hr className={classes.invis} />
+                    <Typography variant="body2" color="textSecondary">
+                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                    </Typography>
+                    <hr className={classes.invis} />
+                    <Typography variant="body1">
+                        {body}
+                    </Typography>
+                    
+                </Grid>
+            </Grid>
+        )
+
+
         return(
-            <h1>h</h1>
+            <React.Fragment>
+                <Tooltip title="Expand scream" onClick={this.handleOpen}>
+                    <IconButton>
+                        <UnfoldMore color="primary" /> 
+                    </IconButton>
+                
+                </Tooltip>
+                    
+                    <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
+                <Tooltip title="close" onClick={this.handleClose}  >
+                    <IconButton >
+                        <CloseIcon  />
+                    </IconButton>
+                  </Tooltip> 
+                  <DialogContent className={classes.DialogContent}>
+                      {dialogMarkUP}
+                  </DialogContent>
+
+                    </Dialog>
+                  
+
+
+            </React.Fragment>
         )
     }
 }
